2225ef304c658cf9826de51a03e2a094a298760d
RodH24 <hm.rodriguezgomez24@gmail.com>
2023-11-14 14:55:47 -0600

    fixed error folder images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImagesComponent } from './components/components/images/images.component';
 import { ImgComponent } from './components/img/img.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImagesComponent,
     ImgComponent
   ],
   imports: [
