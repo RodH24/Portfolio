c63845674c0303d004ed22244164d19788b7d6af
RodH24 <rodh24@DarkOfHeartness.local>
2024-04-03 15:24:00 -0600

    navbar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImgComponent
+    ImgComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
