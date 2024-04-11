2af83a9f58cf7671860e443fec2f6ba9caed916a
RodH24 <rodh24@DarkOfHeartness.local>
2024-04-11 14:48:57 -0600

    created about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ImgComponent } from './components/img/img.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './components/header/header.component';
     ImgComponent,
     NavbarComponent,
     BannerComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent {
+  face: string = 'https://media.licdn.com/dms/image/D5635AQHxubxHPyQaDg/profile-framedphoto-shrink_200_200/0/1684786537656?e=1713474000&v=beta&t=0jUIwrDjl0mHhirTZ2fTYPdchIdvSt_au8Bafblqt9U';
+
+}
