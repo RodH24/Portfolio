583603ef9e9933c8fa0bf9e6bb5da346f691e798
RodH24 <rodh24@DarkOfHeartness.local>
2024-04-16 13:23:10 -0600

    created models section

--- src/app/components/works-done/works-done.component.ts
+++ src/app/components/works-done/works-done.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { WorkModel } from 'src/app/models/work.model';
+import { WORKS } from 'src/assets/files/mock-works';
 
 @Component({
   selector: 'app-works-done',
@@ -8,24 +10,5 @@ import { Component, Input } from '@angular/core';
 export class WorksDoneComponent {
   @Input() title: string = 'Trabajos';
 
-  workList = [
-    {
-      "id": 1,
-      "title": "Primer Trabajo",
-      "urlImage": "https://cyberzon3.com/assets/cyber_zon3_logo-ad3d2abb.png",
-      "description": "Me encanto este trabajo"
-    },
-    {
-      "id": 2,
-      "title": "Segundo Trabajo",
-      "urlImage": "https://cyberzon3.com/assets/cyber_zon3_logo-ad3d2abb.png",
-      "description": "Me encanto este trabajo tambien"
-    },
-    {
-      "id": 3,
-      "title": "Tercer Trabajo",
-      "urlImage": "https://cyberzon3.com/assets/cyber_zon3_logo-ad3d2abb.png",
-      "description": "Me encanto este trabajo al igual que los otros"
-    }
-  ]
+  workList: WorkModel[] = WORKS;
 }
