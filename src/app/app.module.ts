15b0c0989ad18b9261b86056256df41acc4893d8
RodH24 <rodh24@DarkOfHeartness.local>
2024-04-18 12:25:48 -0600

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { WorksDoneComponent } from './components/works-done/works-done.component';
 import { WorkComponent } from './components/work/work.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { WorkComponent } from './components/work/work.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
